d94771fc20e5c1edf2f76cb2d15dbf236b740633
Eric Matthes <ehmatthes@gmail.com>
2024-04-22 14:29:50 -0800

    Reads data file.

--- explore_ir_data.py
+++ explore_ir_data.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 
 path = Path(__file__).parent / "data" / "irva_akdt_022016-033124_arch_format.txt"
-# df = pd.read_csv(path, sep="\t")
+df = pd.read_csv(path, sep="\t")
 # breakpoint()
 
 # df.shape
@@ -19,13 +19,13 @@ path = Path(__file__).parent / "data" / "irva_akdt_022016-033124_arch_format.txt
 
 
 
-df = pd.read_csv(path, 
-                 sep='\t',            # Tab-separated values
-                 skiprows=31,         # Adjust this number based on where the data starts in your file
-                 parse_dates=['datetime'], # Parse the 'datetime' column as datetime objects
-                 infer_datetime_format=True, # Infers the format of datetime strings
-                 dtype={'agency_cd': str, 'site_no': str, 'tz_cd': str,
-                        '1343_00065': float, '1343_00065_cd': str,
-                        '1344_00060': float, '1344_00060_cd': str}) # Specify data types for safety
+# df = pd.read_csv(path, 
+#                  sep='\t',            # Tab-separated values
+#                  skiprows=31,         # Adjust this number based on where the data starts in your file
+#                  parse_dates=['datetime'], # Parse the 'datetime' column as datetime objects
+#                  infer_datetime_format=True, # Infers the format of datetime strings
+#                  dtype={'agency_cd': str, 'site_no': str, 'tz_cd': str,
+#                         'gage_height_ft': float, 'status_gage_height': str,
+#                         'discharge_cfs': float, 'status_discharge': str}) # Specify data types for safety
 
 breakpoint()
