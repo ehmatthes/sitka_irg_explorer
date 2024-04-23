7393da662708ae1668ea3ab98508513f7ba22c9b
Eric Matthes <ehmatthes@gmail.com>
2024-04-22 17:59:06 -0800

    Validation on sliders.

--- explore_ir_data.py
+++ explore_ir_data.py
@@ -8,36 +8,47 @@ import streamlit as st
 import pandas as pd
 
 
-def parse_dates_tz(ts):
-    return pd.to_datetime(ts).tz_localize("America/Anchorage", ambiguous="NaT")
-
-
 path = Path(__file__).parent / "data" / "irva_akdt_022016-033124_arch_format.txt"
 df = pd.read_csv(
     path,
     sep="\t",
     parse_dates=["ts_reading"],
-    date_parser=parse_dates_tz,
+    # date_parser=parse_dates_tz,
+    date_format="%Y-%m-%d %H:%M",
     on_bad_lines="skip",
 )
+
+# Set time zone correctly.
+df['ts_reading'] = df['ts_reading'].dt.tz_localize("America/Anchorage", ambiguous="NaT")
 
 first_date = df.ts_reading[0]
 last_date = df.ts_reading.iloc[-1]
 
 # Let user choose start and end date for analysis.
 # Resolution = 7 days.
-slider_date = first_date
-slider_dates = []
-while slider_date <= last_date:
-    slider_dates.append(slider_date)
-    slider_date += datetime.timedelta(days=7)
-
-
-# breakpoint()
+min_value=datetime.date(first_date.year, first_date.month, first_date.day)
+max_value=datetime.date(last_date.year, last_date.month, last_date.day)
+value=datetime.date(2019, 8, 1)
+step=datetime.timedelta(days=7)
 
 start_date = st.slider(
     label="Start date",
-    min_value=datetime.date(first_date.year, first_date.month, first_date.day),
-    max_value=datetime.date(last_date.year, last_date.month, last_date.day)
-    # step=datetime.timedelta(days=7),
+    value=datetime.date(2019, 8, 1),
+    min_value=min_value,
+    max_value=max_value,
+    step=step,
     )
+
+end_date = st.slider(
+    label="End date",
+    value=datetime.date(2019, 8, 31),
+    min_value=min_value,
+    max_value=max_value,
+    step=step,
+    )
+
+if start_date > end_date:
+    st.error("End date must be later than start date.")
+    st.stop()
+
+st.write("---")
