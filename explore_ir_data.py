28bd77ef20766b732a6c049462512c2611350a7b
Eric Matthes <ehmatthes@gmail.com>
2024-04-22 17:06:00 -0800

    Data cleaning; spaces to tabs.

--- explore_ir_data.py
+++ explore_ir_data.py
@@ -8,5 +8,20 @@ import pandas as pd
 
 
 path = Path(__file__).parent / "data" / "irva_akdt_022016-033124_arch_format.txt"
-df = pd.read_csv(path, sep="\t")
+df = pd.read_csv(path, sep="\t",
+    parse_dates=["ts_reading"],
+    # dtype={
+    #     "agency_cd": str,
+    #     "site_no": str,
+    #     "tz_cd": str,
+    #     "gage_height_ft": float,
+    #     "status_gage_height": str,
+    #     "discharge_cfs": float,
+    #     "status_discharge": str,
+    # },
+    on_bad_lines="skip")
 
+# first_date = df.ts_reading[0]
+# last_date = df.ts_reading.iloc[-1]
+
+breakpoint()
