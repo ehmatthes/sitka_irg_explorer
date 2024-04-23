167da6b57ec3fe3791e28cfbaa665455c8395c6c
Eric Matthes <ehmatthes@gmail.com>
2024-04-22 17:21:20 -0800

    Data should be read in so that it's usable; skipping AKDT/DKST transition hours.

--- explore_ir_data.py
+++ explore_ir_data.py
@@ -7,21 +7,16 @@ import streamlit as st
 import pandas as pd
 
 
+def parse_dates_tz(ts):
+    return pd.to_datetime(ts).tz_localize("America/Anchorage", ambiguous="NaT")
+
 path = Path(__file__).parent / "data" / "irva_akdt_022016-033124_arch_format.txt"
 df = pd.read_csv(path, sep="\t",
     parse_dates=["ts_reading"],
-    # dtype={
-    #     "agency_cd": str,
-    #     "site_no": str,
-    #     "tz_cd": str,
-    #     "gage_height_ft": float,
-    #     "status_gage_height": str,
-    #     "discharge_cfs": float,
-    #     "status_discharge": str,
-    # },
+    date_parser=parse_dates_tz,
     on_bad_lines="skip")
 
-# first_date = df.ts_reading[0]
-# last_date = df.ts_reading.iloc[-1]
+first_date = df.ts_reading[0]
+last_date = df.ts_reading.iloc[-1]
 
 breakpoint()
