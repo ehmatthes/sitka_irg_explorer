62dea842e791fa3b6d7c0ac003e836f542286155
Eric Matthes <ehmatthes@gmail.com>
2024-04-23 08:43:50 -0800

    Gets readings from df.

--- utils/explore_utils.py
+++ utils/explore_utils.py
@@ -1,7 +1,28 @@
 """Utils specific to this explorer tool."""
 
-# import utils.ir_reading as ir_reading
+import streamlit as st
+
+import utils.ir_reading as ir_reading
 
 def get_readings_from_df(df):
     """Parse readings df, and return list of Reading objects."""
-    pass
+
+    # Make a single reading object:
+    #   reading = ir_reading.IRReading(dt_utc, height)
+
+    heights = df.gage_height_ft
+    timestamps = df.ts_reading
+    timestamps_utc = df["ts_reading"].dt.tz_convert("UTC")
+    assert len(heights) == len(timestamps_utc)
+
+    st.write(timestamps[0], timestamps_utc[0])
+
+    readings = [
+        ir_reading.IRReading(ts, height)
+        for ts, height
+        in zip(timestamps_utc, heights)
+    ]
+
+    assert len(readings) == len(heights)
+
+    return readings
