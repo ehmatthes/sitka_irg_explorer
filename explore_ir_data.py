b119949effd77a8dd9b4184aa880615c42ce53e6
Eric Matthes <ehmatthes@gmail.com>
2024-04-22 17:21:38 -0800

    Black.

--- explore_ir_data.py
+++ explore_ir_data.py
@@ -10,11 +10,15 @@ import pandas as pd
 def parse_dates_tz(ts):
     return pd.to_datetime(ts).tz_localize("America/Anchorage", ambiguous="NaT")
 
+
 path = Path(__file__).parent / "data" / "irva_akdt_022016-033124_arch_format.txt"
-df = pd.read_csv(path, sep="\t",
+df = pd.read_csv(
+    path,
+    sep="\t",
     parse_dates=["ts_reading"],
     date_parser=parse_dates_tz,
-    on_bad_lines="skip")
+    on_bad_lines="skip",
+)
 
 first_date = df.ts_reading[0]
 last_date = df.ts_reading.iloc[-1]
